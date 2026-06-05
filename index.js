9548f9c8fcaaa95e1508f21060001209fc0b69ac
josuesoares64 <josuesoares318@gmail.com>
2026-06-05 18:12:38 -0300

    Primeiros passos em testes

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+export const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-const calculaDescontos = (salario, descontos) => salario - descontos;
-
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
-  }
-};
-
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
-
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
+export const calculaDescontos = (salario, descontos) => salario - descontos;
